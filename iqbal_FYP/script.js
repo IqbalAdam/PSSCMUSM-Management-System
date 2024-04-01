cac43644a9608a99031077f256b32a968bd6766a
Iqbal Adam <98652994+IqbalAdam@users.noreply.github.com>
2024-04-01 15:47:56 +0800

    Add files via upload

--- iqbal_FYP/script.js
+++ iqbal_FYP/script.js
@@ -99,5 +99,32 @@ rows.forEach(row => {
     dateCell.textContent = formattedDate;
 });
 
+//---------------------------------------------------------------------------------------------------------------------------
+//                                                 Script in manage.html
+//---------------------------------------------------------------------------------------------------------------------------
 
+function clearSearch() {
+    document.getElementById("searchInput").value = ""; // Clear the search input value
+    searchTable(); // Optionally, you can call the searchTable() function to reset the search results
+}
+
+function searchTable() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tableBody");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td");
+        for (var j = 0; j < td.length; j++) {
+            txtValue = td[j].textContent || td[j].innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+                break;
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
 
